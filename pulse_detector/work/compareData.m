aee21a3acfdbac0c37b421710ea105851e8d42aa
Curie Chung <14336981+curiechung@users.noreply.github.com>
2020-02-13 23:13:21 -0500

    Add R2018a tutorial files

---
+++ pulse_detector/work/compareData.m
@@ -0,0 +1,74 @@
+function err_vec = compareData(reference,actual,figure_number,textstring)
+
+% Vector input only
+if ~isvector(reference) || ~isvector(actual)
+    error('Input signals must be vector');
+else
+    if isrow(reference)
+        reference = transpose(reference);
+    end
+    if isrow(actual)
+        actual = transpose(actual);
+    end
+end
+
+% Make signals same length if necessary
+if length(reference) ~= length(actual)
+%     warning(['Length of reference (%d) is not the same as actual signal (%d).'...
+%         ' Truncating the longer input.'],length(reference),length(actual));
+    len = 1:min(length(reference),length(actual));
+    reference = reference(len);
+    actual = actual(len);
+end
+
+% Turn complex into vector
+if xor(isreal(reference),isreal(actual))
+    error('Input signals are not both real or both complex');
+elseif ~isreal(reference)
+    ref_vec = double([real(reference) imag(reference)]);
+    act_vec = double([real(actual) imag(actual)]);
+    tag = {'(Real)','(Imag)'};
+else
+    ref_vec = double(reference);
+    act_vec = double(actual);
+    tag = {''};
+end
+
+% Configure figure
+if iscell(figure_number)
+    if size(ref_vec,2) > 1 % complex
+        error('Cannot yet subplot multiple complex inputs');
+    else
+        figure(figure_number{1})
+    end
+else
+    figure(figure_number)
+end
+c = get(groot,'defaultAxesColorOrder');
+
+% Compute error
+err_vec = ref_vec - act_vec;
+max_err = max(abs(err_vec));
+max_ref = max(abs(ref_vec));
+fprintf('\nMaximum error for %s out of %d values\n',textstring,length(actual));
+
+
+for n = 1:size(ref_vec,2)
+    fprintf('%s %d (absolute), %d (percentage)\n',tag{n},max_err(n),max_err(n)/max_ref(n)*100);
+    if iscell(figure_number)
+        total_plot = figure_number{2};
+        plot_num = figure_number{3};
+    else
+        total_plot = size(ref_vec,2);
+        plot_num = n;
+    end
+    subplot(total_plot,1,plot_num)
+    plot(ref_vec(:,n),'Color',c(3,:));
+    hold on
+    plot(act_vec(:,n),'Color',c(1,:));
+    plot(err_vec(:,n),'Color',c(2,:));
+    legend('Reference','Actual','Error')
+    hold off
+    title(sprintf('%s %s, max error = %.3d',textstring,tag{n},max_err(n)));
+end
+end
---
+++ pulse_detector/work/demoMode.m
@@ -0,0 +1,37 @@
+function demoMode(varargin)
+% demoMode or demoMode('on') adds the folder containing the solution models
+% to MATLAB path. The folder is not added if any of the models is already
+% on the path.
+%
+% demoMode('off') removes the folder from MATLAB path.
+
+if nargin == 0
+    mode_flag = 'on';
+elseif nargin == 1
+    mode_flag = varargin{:};
+else
+    error('Too many input arguments, only 0 or 1 expected.');
+end
+
+switch lower(mode_flag)
+    case 'on'
+        m1 = which('pulse_detector_v1.slx');
+        m2 = which('pulse_detector_v2.slx');
+        m3 = which('pulse_detector_v3.slx');
+        if ~isempty(m1)
+            error('Model already exists:\n%s',m1); 
+        elseif ~isempty(m2)
+            error('Model already exists:\n%s',m2); 
+        elseif ~isempty(m3)
+            error('Model already exists:\n%s',m3); 
+        else
+            addpath('../solution');
+            disp('Added model folder ../solution to path');
+        end
+            
+    case 'off'
+        rmpath('../solution');
+        disp('Removed folder ../solution from path');
+    otherwise
+        error('Undefined input argument');
+end
